e800cb702e4e3cfa89835b6e4802726f5b99a91f
KarenNgala <ngalalalu@gmail.com>
2020-05-08 15:11:16 +0300

    Added code to toggle what we do sections

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#design').click(function() {
+        $('.design').toggle();
+    });
+});
+
+$(document).ready(function() {
+    $('#dvpt').click(function() {
+        $('.dvpt').toggle();
+    });
+});
+
+$(document).ready(function() {
+    $('#product').click(function() {
+        $('.product').toggle();
+    });
+});
