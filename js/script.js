d64ef6a9e9c0e2c43455b4a27ece0e3258f04897
KarenNgala <ngalalalu@gmail.com>
2020-05-10 17:57:34 +0300

    Efforts to make code DRY, failed

--- js/script.js
+++ js/script.js
@@ -27,18 +27,54 @@ $(document).ready(function() {
     });
 
     //portfolio overlays
-    $(".images").mouseover(function() {
-        var images = ["one", "two", "three", "four", "five", "six", "seven", "eight"];
-        images.forEach(function(image) {
-            $("p#" + image).show();
-        });
+    $(".one").mouseover(function() {
+        $("#one").show();
+    });
+    $(".one").mouseout(function() {
+        $("#one").hide();
     });
 
-    $(".images").mouseout(function() {
-        var images = ["one", "two", "three", "four", "five", "six", "seven", "eight"];
-        images.forEach(function(image) {
-            $("p#" + image).hide();
-        });
+    $(".two").mouseover(function() {
+        $("#two").show();
+    });
+    $(".two").mouseout(function() {
+        $("#two").hide();
+    });
+    $(".three").mouseover(function() {
+        $("#three").show();
+    });
+    $(".three").mouseout(function() {
+        $("#three").hide();
+    });
+    $(".four").mouseover(function() {
+        $("#four").show();
+    });
+    $(".four").mouseout(function() {
+        $("#four").hide();
+    });
+    $(".five").mouseover(function() {
+        $("#five").show();
+    });
+    $(".five").mouseout(function() {
+        $("#five").hide();
+    });
+    $(".six").mouseover(function() {
+        $("#six").show();
+    });
+    $(".six").mouseout(function() {
+        $("#six").hide();
+    });
+    $(".seven").mouseover(function() {
+        $("#seven").show();
+    });
+    $(".seven").mouseout(function() {
+        $("#seven").hide();
+    });
+    $(".eight").mouseover(function() {
+        $("#eight").show();
+    });
+    $(".eight").mouseout(function() {
+        $("#eight").hide();
     });
 
     //submit message
